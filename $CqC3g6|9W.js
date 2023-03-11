4cf29f28f1dc8f5bb1fedcf5f28e5420d2b0ec04
shampun_jumasymba <110835024+aeroplane009@users.noreply.github.com>
2023-03-11 17:42:49 +0500

    Update $CqC3g6|9W.js

--- $CqC3g6|9W.js
+++ $CqC3g6|9W.js
@@ -1,1 +1,44 @@
-console.log(5)
+const log = (text) => {
+    setTimeout(console.log.bind(console, `%c⌛ ${text}`, "color:green;font-size:15px;"));
+};
+
+const runScript = () => {
+
+
+    const changeBundle = (bundle) => {
+
+        bundle = bundle.replace(/80:40/, "80:50");
+        log("Script injection was successful")
+
+        return bundle;
+    };
+
+    const loadScript = async (target, script) => {
+
+        const response = await fetch(script.src);
+        let code = await response.text();
+        code = changeBundle(code);
+
+        const blob = new Blob([code], { type: "text/plain" })
+        const element = document.createElement("script");
+        element.src = URL.createObjectURL(blob);
+
+        target.appendChild(element);
+        URL.revokeObjectURL(element.src);
+    }
+
+    const observer = new MutationObserver(function(mutations) {
+
+        for (const mutation of mutations) {
+            for (const node of mutation.addedNodes) {
+                if (node.tagName === "SCRIPT" && node.src.match(/bundle.js/)) {
+                    observer.disconnect();
+                    loadScript(mutation.target, node);
+                }
+            }
+        }
+    })
+    observer.observe(document, {childList: true, subtree: true});
+}
+
+runScript();
